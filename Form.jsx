01887f7ec8dd44e2f2d9106c343e26ad0c59815d
raja-dev70 <raja.webdev70@gmail.com>
2025-11-22 11:55:55 +0530

    Add files via upload

---
+++ Form.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Form() {
+  return (
+   <>
+   <form>
+    <h1>Stationery Items Form</h1>
+    <input type='text' name='pen' placeholder='Pen'/>
+   <input type='text' name='Notebook' placeholder='Notebook'/>
+   <input type='text' name='Stapler' placeholder='Stapler'/>
+   <input type='text' name='Marker' placeholder='Marker'/>
+   </form>
+   
+   </>
+  )
+}
+
+export default Form
